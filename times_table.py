35c63487181083c264c01680817af0babb6f1811
Gatduel Riek Thiep <gatduelriek@gmail.com>
2026-09-24 14:22:23 +0300

    Add files via upload

---
+++ times_table.py
@@ -0,0 +1,9 @@
+# Ask the user to input his / her favorite number
+user = int(input("What is your favorite number? "))
+
+# Iterate using a for loop
+for i in range(1, 11):
+    print(f"Time tables is {user * i}")
+
+print()
+print("This is the end of the for loop")
