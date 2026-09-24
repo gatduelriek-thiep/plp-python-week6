f6df4e12dad68fe6af305ec6a94121f759030cdd
Gatduel Riek Thiep <gatduelriek@gmail.com>
2026-09-24 14:27:41 +0300

    Update skip_counter.py

--- skip_counter.py
+++ skip_counter.py
@@ -3,5 +3,5 @@ for i in range(0, 21, 2):
 print("This is the end of even numbers")
 print()
 print("This is the beginning of countdown")
-for a in range(10, 0, -1):
+for a in range(10, -1, -1):
     print(f"Countdown: {a}")
