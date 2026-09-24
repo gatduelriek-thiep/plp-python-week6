e8bf79692ebc1e9a503eaeedad9abfa95e135d82
Gatduel Riek Thiep <gatduelriek@gmail.com>
2026-09-24 14:29:06 +0300

    Update times table output format

--- times_table.py
+++ times_table.py
@@ -3,7 +3,7 @@ user = int(input("What is your favorite number? "))
 
 # Iterate using a for loop
 for i in range(1, 11):
-    print(f"Time tables is {user * i}")
+    print(f"{user} x {i} = {user * i}")
 
 print()
 print("This is the end of the for loop")
